87e9d619d9ab31dc302a7b3daf260341a06b8f2c
gk <promise_ghostking@163.com>
2026-08-11 15:31:02 +0800

    chore(deps): 添加 CSS 重置和 Element Plus 样式导入

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import '@unocss/reset/tailwind.css'
+import 'element-plus/dist/index.css'
 import 'uno.css'
 
 const app = createApp(App)
